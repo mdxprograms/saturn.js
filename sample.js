81dfbff00576b2853ee42e0938c2780248681c1d
Josh Waller <mdx.programs@gmail.com>
2017-08-13 00:50:42 -0400

    adjusting code demo

--- sample.js
+++ sample.js
@@ -2,10 +2,14 @@ import Saturn from './saturn'
 
 const url = 'https://www.reddit.com/r/javascript.json'
 const btnTrigger = Saturn.orbit('[data-click-me]')
+const stateKey = 'javascript'
 
 btnTrigger.on('click')
-  .then(() => Saturn.fetchData(url, 'javascript'))
-  .then(() => Saturn.render(
-    '[data-render-me]',
-    `<p>${Saturn.state.javascript.data.children[1].data.title}</p>`
+  .then(() => Saturn.fetchData(url, stateKey))
+  .then(() => Saturn.render('[data-render-me]',
+    `
+      <p>
+        ${Saturn.state.javascript.data.children[1].data.title}
+      </p>
+    `
   ))
